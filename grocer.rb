9d00a772717c57dac23c4f12a409b4931d9517d5
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-04 00:38:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,13 @@
 def consolidate_cart(cart)
-  # code here
-end
-
+ final_cart_hash = {}
+  cart.each do |cart_items|
+    cart_items.each do |cart_items, attributes_hash|
+      final_cart_hash[cart_items] ||= attributes_hash
+        final_cart_hash[cart_items][:count]? final_cart_hash[cart_items][:count] += 1 : final_cart_hash[cart_items][:count] = 1
+      end
+    end   
+  end
+  
 def apply_coupons(cart, coupons)
   # code here
 end
--- random_grocery_generator.rb
+++ random_grocery_generator.rb
@@ -1,5 +1,6 @@
 require_relative 'grocer'
 
+
 def items
 	[
 		{"AVOCADO" => {:price => 3.00, :clearance => true}},
